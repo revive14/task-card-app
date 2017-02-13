572e3dfcab392c518baf99c87287f18c33c38cdd
Hamza Shaikh <Santa@Hamzas-MBP.att.net>
2017-02-13 08:57:50 -0600

    added a card component

--- app/card/card.component.js
+++ app/card/card.component.js
@@ -9,21 +9,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-var app_component_1 = require('./app.component');
-var card_component_1 = require("./card/card.component");
-var AppModule = (function () {
-    function AppModule() {
+var CardComponent = (function () {
+    function CardComponent() {
     }
-    AppModule = __decorate([
-        core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
-            declarations: [app_component_1.AppComponent, card_component_1.CardComponent],
-            bootstrap: [app_component_1.AppComponent]
+    CardComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'app-card',
+            templateUrl: 'card.component.html',
+            styleUrls: ['card.component.css']
         }), 
         __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    ], CardComponent);
+    return CardComponent;
 }());
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+exports.CardComponent = CardComponent;
+//# sourceMappingURL=card.component.js.map
